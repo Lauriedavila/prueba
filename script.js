5119f1669f6ccc7680f07616460cdfdf94fd4238
Lauriedavila <davilalaura1990@gmail.com>
2024-05-19 16:26:14 +0200

    Java_tarea_clase_10

---
+++ script.js
@@ -0,0 +1,23 @@
+let nombre = "Pedro";
+let years = 30;
+console.log(years + " " + nombre);
+let numberone=120;
+let numbertwo=20;
+let resultsuma = numberone + numbertwo;
+let resultresta = numberone - numbertwo;
+let resultmulti = numberone * numbertwo;
+let resultdivision = numberone / numbertwo;
+console.log("suma:",resultsuma);
+console.log("Resta:",resultresta);
+console.log("Multiplicación:",resultmulti);
+console.log("División:",resultdivision);
+
+let nombre2 = "Laura";
+let apellido = "Dávila";
+console.log(nombre2+" "+apellido);
+
+let numberthree=129;let numberfour= 19
+let suma2 = numberthree + numberfour;
+console.log("suma2:",suma2);
+let numberfive = suma2 + 38;
+console.log("variable:",numberfive);
